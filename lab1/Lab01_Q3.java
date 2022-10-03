56d052ab7579047ae8ae05489200c55708ed20b7
Emirhan Yagcioglu <114405905+EmirhanYagcioglu@users.noreply.github.com>
2022-10-03 18:46:35 +0300

    Update Lab01_Q3.java

--- lab1/Lab01_Q3.java
+++ lab1/Lab01_Q3.java
@@ -1,27 +1,27 @@
 public class Lab01_Q3 {
     public static void main(String[] args) {
-        final double MASS_PERCENTAGE_OF_OXYGEN       = 65.0;
-        final double MASS_PERCENTAGE_OF_CARBON       = 18.5;
-        final double MASS_PERCENTAGE_OF_HYDROGEN     = 10.0;
-        final double MASS_PERCENTAGE_OF_NITROGEN     = 3.2;
-        final double MASS_PERCENTAGE_OF_CALCIUM      = 1.5;
+        final double MASS_PERCENTAGE_OF_OXYGEN          = 65.0;
+        final double MASS_PERCENTAGE_OF_CARBON          = 18.5;
+        final double MASS_PERCENTAGE_OF_HYDROGEN        = 10.0;
+        final double MASS_PERCENTAGE_OF_NITROGEN        = 3.2;
+        final double MASS_PERCENTAGE_OF_CALCIUM         = 1.5;
 
-        final double MASS_PERCENTAGE_OF_FIVE_ELEMENTS      = MASS_PERCENTAGE_OF_OXYGEN + MASS_PERCENTAGE_OF_CARBON + MASS_PERCENTAGE_OF_HYDROGEN + MASS_PERCENTAGE_OF_NITROGEN + MASS_PERCENTAGE_OF_CALCIUM;
-        final double MASS_PERCENTAGE_OF_PHOSPORUS    = 98.6 - MASS_PERCENTAGE_OF_FIVE_ELEMENTS;
-        final double MASS_PERCENTAGE_FOR_REMAINDER   = 100.0 - 98.6;
+        final double MASS_PERCENTAGE_OF_FIVE_ELEMENTS   = MASS_PERCENTAGE_OF_OXYGEN + MASS_PERCENTAGE_OF_CARBON + MASS_PERCENTAGE_OF_HYDROGEN + MASS_PERCENTAGE_OF_NITROGEN + MASS_PERCENTAGE_OF_CALCIUM;
+        final double MASS_PERCENTAGE_OF_PHOSPORUS       = 98.6 - MASS_PERCENTAGE_OF_FIVE_ELEMENTS;
+        final double MASS_PERCENTAGE_FOR_REMAINDER      = 100.0 - 98.6;
 
-        final double COUNT_PERCENTAGE_OF_HYDROGEN = 62.0;
-        final double COUNT_PERCENTAGE_OF_OXYGEN = 24.0;
+        final double COUNT_PERCENTAGE_OF_HYDROGEN       = 62.0;
+        final double COUNT_PERCENTAGE_OF_OXYGEN         = 24.0;
 
-        double massOfTheHuman = 70.0;
-        double oxygenMass = massOfTheHuman * MASS_PERCENTAGE_OF_OXYGEN / 100;
-        double carbonMass = massOfTheHuman * MASS_PERCENTAGE_OF_CARBON / 100;
+        double massOfTheHuman       = 70.0;
+        double oxygenMass           = massOfTheHuman * MASS_PERCENTAGE_OF_OXYGEN / 100;
+        double carbonMass           = massOfTheHuman * MASS_PERCENTAGE_OF_CARBON / 100;
 
-        double atomCountTotal     = massOfTheHuman * 1.0E26;
-        double atomCountRemainder = MASS_PERCENTAGE_FOR_REMAINDER * atomCountTotal / 100;
+        double atomCountTotal       = massOfTheHuman * 1.0E26;
+        double atomCountRemainder   = MASS_PERCENTAGE_FOR_REMAINDER * atomCountTotal / 100;
 
-        double hydrogenCount = atomCountTotal * COUNT_PERCENTAGE_OF_HYDROGEN / 100;
-        double oxygenCount = atomCountTotal * COUNT_PERCENTAGE_OF_OXYGEN / 100;
+        double hydrogenCount        = atomCountTotal * COUNT_PERCENTAGE_OF_HYDROGEN / 100;
+        double oxygenCount          = atomCountTotal * COUNT_PERCENTAGE_OF_OXYGEN / 100;
 
 
         System.out.println( "Phosphorus makes up " + MASS_PERCENTAGE_OF_PHOSPORUS + " percent of the mass in the human body." );
